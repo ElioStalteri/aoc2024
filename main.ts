b433344f416a443a0fcffe02e26e33e84371c0f6
elio <elio@localhost.localdomain>
2024-12-01 11:25:36 +0000

    refactor

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import * as aoc from "./src/index.ts";
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   for (const [name, func] of Object.entries(aoc)) {
-    if (name !== "template") console.log(name, func());
+    if (name === "template") continue;
+    console.log(name, func());
   }
 }
