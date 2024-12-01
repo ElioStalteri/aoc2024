7701d7b6cfe0da2fad3142508a676f70ef3d94b4
elio <elio@localhost.localdomain>
2024-12-01 11:22:36 +0000

    added day 2

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ import * as aoc from "./src/index.ts";
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   for (const [name, func] of Object.entries(aoc)) {
-    console.log(name, func());
+    if (name !== "template") console.log(name, func());
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { solve as template } from "./template/index.ts";
 export { solve as day1 } from "./01/index.ts";
+export { solve as day2 } from "./02/index.ts";
