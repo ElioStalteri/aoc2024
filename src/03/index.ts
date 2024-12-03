c141b919e6eeb852ff3d3605142a902cbe162d16
elio <elio@localhost.localdomain>
2024-12-03 09:34:16 +0000

    solved day 3

--- src/03/index.ts
+++ src/03/index.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-fallthrough
 import { assertEquals } from "@std/assert";
 
 const decoder = new TextDecoder("utf-8");
@@ -8,12 +9,201 @@ const testFile = decoder.decode(
   await Deno.readFile(import.meta.dirname + "/test.txt"),
 );
 
-function part1(_data: string) {
-  return "todo";
+function part1(data: string) {
+  const chars = data.trim().split("");
+  const numbers = "0123456789";
+
+  const state: {
+    prev: null | string;
+    number: null | string;
+    res: number[][];
+    tmp: number[];
+  } = {
+    number: null,
+    prev: null,
+    tmp: [],
+    res: [],
+  };
+
+  for (const c of chars) {
+    switch (c) {
+      case "m":
+        state.prev = "m";
+        break;
+      case "u":
+        if (state.prev === "m") {
+          state.prev = "u";
+          break;
+        }
+      case "l":
+        if (state.prev === "u") {
+          state.prev = "l";
+          break;
+        }
+      case "(":
+        if (state.prev === "l") {
+          state.prev = "(";
+          break;
+        }
+      case ",":
+        if (state.number && state.prev !== null) {
+          state.prev = c;
+          const num = parseInt(state.number);
+          state.number = null;
+          state.tmp.push(num);
+          break;
+        }
+      case ")":
+        if (state.number && state.prev !== null) {
+          const num = parseInt(state.number);
+          state.tmp.push(num);
+          state.res.push(state.tmp);
+          state.prev = null;
+          state.number = null;
+          state.tmp = [];
+        }
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        if (
+          (state.prev && numbers.includes(state.prev)) ||
+          state.prev === "(" ||
+          state.prev === ","
+        ) {
+          state.prev = c;
+          state.number = (state.number || "") + c;
+          break;
+        }
+      default:
+        state.prev = null;
+        state.number = null;
+        state.tmp = [];
+    }
+  }
+
+  return state.res.map(([a, b]) => a * b).reduce((acc, v) => acc + v, 0);
 }
 
-function part2(_data: string) {
-  return "todo";
+function part2(data: string) {
+  const chars = data.trim().split("");
+  const numbers = "0123456789";
+
+  const state: {
+    prev: null | string;
+    number: null | string;
+    res: number[][];
+    tmp: number[];
+    do: boolean;
+    enabled: boolean;
+  } = {
+    enabled: true,
+    do: true,
+    number: null,
+    prev: null,
+    tmp: [],
+    res: [],
+  };
+
+  for (const c of chars) {
+    switch (c) {
+      case "m":
+        if (state.enabled) {
+          state.prev = "m";
+          break;
+        }
+      case "u":
+        if (state.prev === "m") {
+          state.prev = "u";
+          break;
+        }
+      case "l":
+        if (state.prev === "u") {
+          state.prev = "l";
+          break;
+        }
+      case "(":
+        if (state.prev && ["l", "t", "o"].includes(state.prev)) {
+          state.prev = "(";
+          break;
+        }
+      case ",":
+        if (state.number && state.prev !== null) {
+          state.prev = c;
+          const num = parseInt(state.number);
+          state.number = null;
+          state.tmp.push(num);
+          break;
+        }
+      case "0":
+      case "1":
+      case "2":
+      case "3":
+      case "4":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        if (
+          (state.prev && numbers.includes(state.prev)) ||
+          state.prev === "(" ||
+          state.prev === ","
+        ) {
+          state.prev = c;
+          state.number = (state.number || "") + c;
+          break;
+        }
+      case ")":
+        if (state.number && state.prev !== null) {
+          const num = parseInt(state.number);
+          state.tmp.push(num);
+          state.res.push(state.tmp);
+        }
+        if (state.prev === "(") {
+          state.enabled = state.do;
+        }
+      case "d":
+        state.prev = "d";
+        state.number = null;
+        state.tmp = [];
+        break;
+      case "o":
+        if (state.prev === "d") {
+          state.prev = "o";
+          state.do = true;
+          break;
+        }
+      case "n":
+        if (state.prev === "o") {
+          state.prev = "n";
+          break;
+        }
+      case "'":
+        if (state.prev === "n") {
+          state.prev = "'";
+          break;
+        }
+      case "t":
+        if (state.prev === "'") {
+          state.prev = "t";
+          state.do = false;
+          break;
+        }
+      default:
+        state.prev = null;
+        state.number = null;
+        state.tmp = [];
+    }
+  }
+
+  return state.res.map(([a, b]) => a * b).reduce((acc, v) => acc + v, 0);
 }
 
 export function solve() {
@@ -24,9 +214,9 @@ export function solve() {
 }
 
 Deno.test(function part1Test() {
-  assertEquals(part1(testFile), "todo");
+  assertEquals(part1(testFile), 161);
 });
 
 Deno.test(function part2Test() {
-  assertEquals(part2(testFile), "todo");
+  assertEquals(part2(testFile), 48);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { solve as template } from "./template/index.ts";
 export { solve as day1 } from "./01/index.ts";
 export { solve as day2 } from "./02/index.ts";
+export { solve as day3 } from "./03/index.ts";
