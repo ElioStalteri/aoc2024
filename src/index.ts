bd29d1c977ed283fdaf28617f11c0140a349788a
elio <elio@localhost.localdomain>
2024-12-03 09:36:14 +0000

    setup day 4

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { solve as template } from "./template/index.ts";
 export { solve as day1 } from "./01/index.ts";
 export { solve as day2 } from "./02/index.ts";
 export { solve as day3 } from "./03/index.ts";
+export { solve as day4 } from "./04/index.ts";
