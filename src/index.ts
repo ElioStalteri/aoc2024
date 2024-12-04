d5f32b853e6071ebdc48c7a8f40f9b37f2b0a3be
elio <elio@localhost.localdomain>
2024-12-04 09:44:05 +0000

    setup day 5

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { solve as day1 } from "./01/index.ts";
 export { solve as day2 } from "./02/index.ts";
 export { solve as day3 } from "./03/index.ts";
 export { solve as day4 } from "./04/index.ts";
+export { solve as day5 } from "./05/index.ts";
