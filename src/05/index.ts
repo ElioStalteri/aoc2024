006b7c1f45f682a5fa2f5902bbc30ee51ea1b22f
elio <elio@localhost.localdomain>
2024-12-05 09:41:06 +0000

    day 5 part 1 done

--- src/05/index.ts
+++ src/05/index.ts
@@ -8,8 +8,43 @@ const testFile = decoder.decode(
   await Deno.readFile(import.meta.dirname + "/test.txt"),
 );
 
-function part1(_data: string) {
-  return "todo";
+function part1(data: string) {
+  const [rulesString, updatesString] = data.trim().split("\n\n", 2);
+  const rules = new Set(
+    rulesString.split("\n")
+      .map((v) => v.trim())
+      .filter(Boolean),
+  );
+  const updates = updatesString.split("\n")
+    .map((v) => v.trim())
+    .filter(
+      Boolean,
+    ).map((updateStr) => {
+      const update = updateStr.trim().split(",");
+      if (update.length % 2 == 0) {
+        throw new Error("not correct -> " + updatesString);
+      }
+      const middle = parseInt(update[(update.length - 1) / 2]);
+      const updateRev = update.toReversed();
+      const toCheck = new Set<string>();
+      for (let i = 0; i < updateRev.length - 1; i++) {
+        for (let j = i + 1; j < updateRev.length; j++) {
+          toCheck.add(`${updateRev[i]}|${updateRev[j]}`);
+        }
+      }
+      return {
+        middle,
+        toCheck,
+        //updateStr,
+        //intersection: rules.intersection(toCheck),
+      };
+    })
+    .filter(({ toCheck }) => rules.intersection(toCheck).size === 0);
+
+  //console.log(rules, updates);
+
+  return updates
+    .reduce((acc, v) => acc + v.middle, 0);
 }
 
 function part2(_data: string) {
@@ -24,7 +59,7 @@ export function solve() {
 }
 
 Deno.test(function part1Test() {
-  assertEquals(part1(testFile), "todo");
+  assertEquals(part1(testFile), 143);
 });
 
 Deno.test(function part2Test() {
