586fd3a9c9706994e9cadc51f4bd0ab32e689c86
elio <elio@localhost.localdomain>
2024-12-07 11:51:26 +0000

    day 6 start, created a function for the permutation pretty proud of me :)

--- src/07/index.ts
+++ src/07/index.ts
@@ -8,8 +8,43 @@ const testFile = decoder.decode(
   await Deno.readFile(import.meta.dirname + "/test.txt"),
 );
 
-function part1(_data: string) {
-  return "todo";
+enum OP {
+  PLUS = "+",
+  MUL = "*",
+}
+
+const permTable: {
+  [key: number]: OP[][];
+} = {};
+
+function permStep(times: number): OP[][] {
+  if (times in permTable) return permTable[times];
+  const ops = Object.values(OP);
+  if (times === 1) return ops.map((v) => [v]);
+  const nextPerm = permStep(times - 1);
+  let res: OP[][] = [];
+  for (const op of ops) {
+    res = res.concat(nextPerm.map((v) => [op, ...v]));
+  }
+  permTable[times] = res;
+  return res;
+}
+
+function part1(data: string) {
+  const equations = data
+    .trim()
+    .split("\n")
+    .filter(Boolean)
+    .map((l) => {
+      const [res, eq] = l.split(":", 2);
+      return {
+        res: parseInt(res),
+        eq: eq.trim().split(" ").map((n) => parseInt(n)),
+      };
+    });
+
+  console.log(permStep(8).length);
+  return 0;
 }
 
 function part2(_data: string) {
@@ -24,7 +59,7 @@ export function solve() {
 }
 
 Deno.test(function part1Test() {
-  assertEquals(part1(testFile), "todo");
+  assertEquals(part1(testFile), 3749);
 });
 
 Deno.test(function part2Test() {
