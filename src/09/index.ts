93c3b72379b829fa5d407e08a961a6ab3c0e8de2
elio <elio@localhost.localdomain>
2024-12-09 10:41:48 +0000

    solved part 2 day 9

--- src/09/index.ts
+++ src/09/index.ts
@@ -35,8 +35,48 @@ function part1(data: string) {
   );
 }
 
-function part2(_data: string) {
-  return "todo";
+function part2(data: string) {
+  let filesLength = 0;
+  const filesLengthMap: { [key: string]: number } = {};
+  const emptyLengthMap: { [key: string]: number } = {};
+  const memory = data.trim().split("").flatMap((v, i) => {
+    const amount = parseInt(v);
+    const key = i % 2 === 0 ? `${i / 2}` : ".";
+    if (key !== ".") {
+      filesLengthMap[key] = amount;
+    } else {
+      emptyLengthMap[`${(i - 1) / 2}`] = amount;
+    }
+    filesLength += i % 2 === 0 ? amount : 0;
+    return new Array(amount).fill(key);
+  });
+
+  const resMemory = Array.from(memory);
+  let currFile;
+  for (let i = 0; i < memory.length; i++) {
+    const val = memory[i];
+    if (val === ".") {
+      for (let j = memory.length - 1; j > i; j--) {
+        if (resMemory[j] !== ".") {
+          if (filesLengthMap[memory[j]] <= emptyLengthMap[currFile]) {
+            filesLengthMap[resMemory[j]] -= 1;
+            emptyLengthMap[currFile] -= 1;
+            resMemory[i] = resMemory[j];
+            resMemory[j] = ".";
+            break;
+          }
+        }
+      }
+    } else {
+      currFile = val;
+    }
+  }
+
+  return resMemory.map((v, i) => v === "." ? 0 : parseInt(v) * i)
+    .reduce(
+      (acc, v) => acc + v,
+      0,
+    );
 }
 
 export function solve() {
@@ -51,5 +91,5 @@ Deno.test(function part1Test() {
 });
 
 Deno.test(function part2Test() {
-  assertEquals(part2(testFile), "todo");
+  assertEquals(part2(testFile), 2858);
 });
