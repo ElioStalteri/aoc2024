0bbfb63fa0efc8c661b0a57bde74714ecadb755d
elio <elio@localhost.localdomain>
2024-12-09 10:13:31 +0000

    solved day 9 part 1

--- src/09/index.ts
+++ src/09/index.ts
@@ -8,8 +8,31 @@ const testFile = decoder.decode(
   await Deno.readFile(import.meta.dirname + "/test.txt"),
 );
 
-function part1(_data: string) {
-  return "todo";
+function part1(data: string) {
+  let filesLength = 0;
+  const memory = data.trim().split("").flatMap((v, i) => {
+    const amount = parseInt(v);
+    filesLength += i % 2 === 0 ? amount : 0;
+    return new Array(amount).fill(i % 2 === 0 ? `${i / 2}` : ".");
+  });
+
+  for (let i = 0; i <= filesLength; i++) {
+    const val = memory[i];
+    if (val === ".") {
+      for (let j = memory.length - 1; j > i; j--) {
+        if (memory[j] !== ".") {
+          memory[i] = memory[j];
+          memory[j] = ".";
+          break;
+        }
+      }
+    }
+  }
+
+  return memory.filter((v) => v !== ".").map((v, i) => parseInt(v) * i).reduce(
+    (acc, v) => acc + v,
+    0,
+  );
 }
 
 function part2(_data: string) {
@@ -24,7 +47,7 @@ export function solve() {
 }
 
 Deno.test(function part1Test() {
-  assertEquals(part1(testFile), "todo");
+  assertEquals(part1(testFile), 1928);
 });
 
 Deno.test(function part2Test() {
