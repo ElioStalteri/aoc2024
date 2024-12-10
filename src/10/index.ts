7f88de0f074f59e99576c0a7925af608df04f7c4
elio <elio@localhost.localdomain>
2024-12-10 11:56:08 +0000

    solver day 10 part 2

--- src/10/index.ts
+++ src/10/index.ts
@@ -24,6 +24,10 @@ const positionsStorage: {
   [key: string]: P[];
 } = {};
 
+function stringPos(p: P) {
+  return `${p.x},${p.y}`;
+}
+
 function checkPos(arr: number[][], p: P, nines: P[] = []) {
   const curr = arr?.[p.y]?.[p.x];
   if (curr === undefined) return nines;
@@ -32,8 +36,8 @@ function checkPos(arr: number[][], p: P, nines: P[] = []) {
     if (found) return nines;
     return [...nines, p];
   }
-  if (`${p.x},${p.y}` in positionsStorage) {
-    return [...nines, ...positionsStorage[`${p.x},${p.y}`]];
+  if (stringPos(p) in positionsStorage) {
+    return [...nines, ...positionsStorage[stringPos(p)]];
   }
   return nines;
 }
@@ -42,6 +46,12 @@ function allowedNext(arr: number[][], currP: P, nextP: P) {
   const curr = arr?.[currP.y]?.[currP.x];
   const next = arr?.[nextP.y]?.[nextP.x];
   return curr !== undefined && next !== undefined && curr + 1 === next;
+}
+
+function allowedPrev(arr: number[][], currP: P, nextP: P) {
+  const curr = arr?.[currP.y]?.[currP.x];
+  const next = arr?.[nextP.y]?.[nextP.x];
+  return curr !== undefined && next !== undefined && curr === next + 1;
 }
 
 function dedupeNines(n1: P[], n2: P[]) {
@@ -63,9 +73,50 @@ function search(arr: number[][], p: P, nines: P[] = []) {
     if (!allowedNext(arr, p, newP)) continue;
     newNines = search(arr, { x: p.x + d.x, y: p.y + d.y }, newNines);
   }
-  positionsStorage[`${p.x},${p.y}`] = newNines;
+  positionsStorage[stringPos(p)] = newNines;
   return dedupeNines(nn, newNines);
 }
+
+function searchPaths(arr: number[][], start: P, end: P) {
+  const res: { [key: string]: any } = {};
+  for (const d of directions) {
+    const newP = { x: end.x + d.x, y: end.y + d.y };
+    const newPV = arr?.[newP.y]?.[newP.x];
+    if (!allowedPrev(arr, end, newP)) continue;
+    if (newP.x === start.x && newP.y === start.y) {
+      return {
+        [stringPos(end)]: stringPos(start),
+      };
+    }
+    if (newPV === 0) continue;
+    res[stringPos(newP)] = searchPaths(arr, start, newP);
+  }
+  return res;
+}
+
+interface OBJ {
+  [key: string]: OBJ | string;
+}
+
+function getTails(obj: OBJ | string): string[] {
+  if (typeof obj === "string" || obj instanceof String) return [obj as string];
+  const values = Object.entries(obj);
+  return values.map(([k, v]) => getTails(v).map((s) => `${k}|${s}`)).flat();
+}
+
+const getStringPaths = (arr: number[][], start: P, end: P) =>
+  getTails({
+    [stringPos(end)]: searchPaths(arr, start, end),
+  });
+
+const computePathSet = (arr: number[][], correctTrail: {
+  start: P;
+  ends: P[];
+}) =>
+  new Set(
+    correctTrail.ends.map((end) => getStringPaths(arr, correctTrail.start, end))
+      .flat().toSorted(),
+  );
 
 function part1(data: string) {
   const trails = data.trim().split("\n").map((r) =>
@@ -77,12 +128,27 @@ function part1(data: string) {
 
   const correctTrails = zeros.map((p) => search(trails, p));
 
-  //console.log(correctTrails);
   return correctTrails.map((t) => t.length).reduce((acc, v) => acc + v, 0);
 }
 
-function part2(_data: string) {
-  return "todo";
+function part2(data: string) {
+  const trails = data.trim().split("\n").map((r) =>
+    r.trim().split("").map((v) => parseInt(v))
+  ).filter(Boolean);
+  const zeros = trails.flatMap((r, y) =>
+    r.map((v, x) => v === 0 ? { x, y } : false).filter(Boolean)
+  ) as P[];
+
+  const correctTrails = zeros.map((p) => ({
+    start: p,
+    ends: search(trails, p),
+  }));
+
+  const paths = correctTrails.map((trail) =>
+    computePathSet(trails, trail).size
+  );
+
+  return paths.reduce((acc, v) => acc + v, 0);
 }
 
 export function solve() {
@@ -97,5 +163,5 @@ Deno.test(function part1Test() {
 });
 
 Deno.test(function part2Test() {
-  assertEquals(part2(testFile), "todo");
+  assertEquals(part2(testFile), 81);
 });
