977b74e8ff8a6501c9d1a33519371d1841fd0bc9
elio <elio@localhost.localdomain>
2024-12-16 17:03:13 +0000

    added ability to run only one day

--- justfile
+++ justfile
@@ -1,9 +1,14 @@
+set positional-arguments
+
 default:
 	just --list
 
 dev:
 	deno task dev
-run:
-	deno run --allow-read main.ts
+
+@run day='':
+	deno run --allow-read main.ts $1
+
 test:
 	deno test --allow-read  src/**/*
+
--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 import * as aoc from "./src/index.ts";
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
+  const day = Deno.args[0];
+  console.log("day", day);
   const files = Object.entries(aoc).sort((a, b) =>
     parseInt(a[0].split("day")[1]) - parseInt(b[0].split("day")[1])
-  );
+  ).filter(([name]) => !day || day === name);
   for (const [name, func] of files) {
     if (name === "template") continue;
     const start = performance.now();
